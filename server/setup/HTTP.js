342398843709ea7492138bc4821e05a6fbd3bbd1
Ryan Keller <rykeller@uw.edu>
2019-01-26 11:04:11 -0800

    Fixed HTTP bug

--- server/setup/HTTP.js
+++ server/setup/HTTP.js
@@ -49,5 +49,5 @@ const HTTPS = (server, config) => {
 /*
 EXPORTS:
 */
-module.exports = process.env.NODE_ENV === 'development' ? HTTP : HTTPS
-// module.exports = HTTP
+// module.exports = process.env.NODE_ENV === 'development' ? HTTP : HTTPS
+module.exports = HTTP
